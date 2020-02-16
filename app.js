1ba3706868da5f5c4deb340733bb9cc5b5435594
Levi Santos R <levi-sr@outlook.com>
2020-02-16 12:37:05 -0300

    Added the save Data to localStorage and function to populate UI with the Saved Data

--- app.js
+++ app.js
@@ -5,30 +5,70 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = parseInt(movieSelect.value);
 
-//Update total and count
+//Get Data from localStorage and populateUI
+const populateUI = () => {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
 
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+
+    const selectedMoviePrice = localStorage.getItem('selectedMoviePrice');
+    if (selectedMoviePrice !== null) {
+        ticketPrice = +selectedMoviePrice;
+    }
+}
+//Populate UI
+populateUI();
+
+//Save Selected Movie Index and Price
+setMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+//Update total and count
 updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    
+
+    //copy selected seats into array and map throuh array returning a new array indexes
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+
 //Movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value //o sinal de + esta fazendo um parse para inteiro
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 
 })
 
 //Seat click event
 container.addEventListener('click', e => {
-    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected');
     }
 
     updateSelectedCount();
 })
 
+//initial count and total set
+updateSelectedCount();
